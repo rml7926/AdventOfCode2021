86ba892d20544bf32aa2cb2f6a8bb841502142a7
Ryan Langley <rml7926@gmail.com>
2021-12-04 12:49:38 -0500

    Set up for stage 2 of day 1 sonar sweep

--- Day1SonarSweep.py
+++ Day1SonarSweep.py
@@ -1,8 +1,10 @@
+#set up
 depth_increase = 0
 
 input_file = open('SonarSweepPuzzleInput.txt','r')
 readings = input_file.readlines()
 
+#part 1
 reading1 = 500
 for reading in readings:
     reading2 = int(reading)
@@ -11,3 +13,6 @@ for reading in readings:
     reading1 = reading2
 
 print ("The number of times the readings increased is:", depth_increase)
+
+#part 2
+print (len(readings))
