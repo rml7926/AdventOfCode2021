5b3f29469e07b141fccfb40ab24040e1abbacb8b
Ryan Langley <rml7926@gmail.com>
2021-12-05 19:00:21 -0500

    Completed day 2 dive part 2 and solved the puzzle :)

--- Day2Dive.py
+++ Day2Dive.py
@@ -20,3 +20,23 @@ for movement in movements:
 
 total_movement = forward_count * (down_count - up_count)
 print (total_movement)
+
+#part 2
+forward_count = 0
+aim = 0
+depth = 0
+
+for movement in movements:
+    command_fragments = movement.split()
+    direction = command_fragments[0]
+    amount = int(command_fragments[1])
+    if direction == "forward":
+        forward_count = forward_count + amount
+        depth = depth + (aim * amount)
+    elif direction == "down":
+        aim = aim + amount
+    else:
+        aim = aim - amount
+
+total = forward_count * depth
+print ("new total:", total)
